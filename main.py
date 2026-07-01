803c09f341a904c7f2426926c21a2c0cc1f63901
pyco25 <max.yatcuk@gmail.com>
2026-07-01 13:48:38 +0200

    Use env variables and create Procfile

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
+import os
 
 
 '''
@@ -50,7 +51,7 @@ This will install the packages from the requirements.txt for this project.
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -86,7 +87,7 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -317,7 +318,7 @@ def contact():
 #         data = request.form
 #         send_email(data["name"], data["email"], data["phone"], data["message"])
 #         return render_template("contact.html", msg_sent=True)
-#     return render_template("contact.html", msg_sent=False)
+#     return render_template("contact.html", msg_sent=False)True
 #
 #
 # def send_email(name, email, phone, message):
@@ -329,4 +330,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=False, port=5001)
